aedf17de937c511cb89f1abae826f20160b28ec7
Al <Al>
2016-03-16 02:11:28 +0300

    Shows where (roughly) selected marker is graphically

--- app/src/main/java/com/example/al/cameramarkersoverlay/FragmentMain.java
+++ app/src/main/java/com/example/al/cameramarkersoverlay/FragmentMain.java
@@ -2,7 +2,11 @@ package com.example.al.cameramarkersoverlay;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -15,8 +19,11 @@ import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 /**
@@ -28,8 +35,17 @@ public class FragmentMain extends Fragment implements SensorEventListener {
 
     private static final long POLLING_FREQ = 1000 * 10;
     private static final float MIN_DISTANCE = 10.0f;
-    public static final float LOW_PASS_PERCENT = 0.85f;
-//    private static final int UPDATE_THRESHOLD = 500;
+    private static final float LOW_PASS_PERCENT = 0.85f;
+    private static final int SHOW_NOTHING = 0;
+    private static final int SHOW_MOSCOW = 1;
+    private static final int SHOW_TOMSK = 2;
+
+    private static final double FIELD_OF_VIEW = 40.0;
+    private static final double ORIENTATION_CORRECTION = 90.0;
+    private static final double IDEAL_ROLL = 90.0;
+    private static final double ROLL_TOLERANCE = 10.0;
+    private static final double PITCH_TOLERANCE = 10.0;
+
 
     private static final Location LOCATION_MOSCOW;
     private static final Location LOCATION_TOMSK;
@@ -60,13 +76,18 @@ public class FragmentMain extends Fragment implements SensorEventListener {
     private TextView mAzimuthTomskTextView;
     private TextView mOrientationTextView;
 
-//    private long mLastUpdateAccelerometer;
-//    private long mLastUpdateMagnetometer;
+    private FrameLayout mFrameLayout;
+    private OverlayView mOverlayView;
 
     // Storage for Sensor readings
     private float[] mGravity = null;
     private float[] mGeomagnetic = null;
     private float[] mOrientationAverage;
+
+    private double mRoll;
+    private int mCurrentOverlay = SHOW_NOTHING;
+    private double mAzimuthMoscow;
+    private double mAzimuthTomsk;
 
     public FragmentMain() {
         //nothing to see here
@@ -109,6 +130,134 @@ public class FragmentMain extends Fragment implements SensorEventListener {
         mOrientationAverage = new float[3];
     }
 
+    private void updateLocationTextView() {
+        if (mLocation != null) {
+            mLocationTextView.setText(String.format(mContext.getString(R.string.format_location),
+                    mLocation.getLatitude(),
+                    mLocation.getLongitude()));
+            mLocationMoscowTextView.setText(String.format(mContext.getString(R.string.format_location_bearings),
+                    LOCATION_MOSCOW.getLatitude(),
+                    LOCATION_MOSCOW.getLongitude(),
+                    mLocation.bearingTo(LOCATION_MOSCOW),
+                    mLocation.distanceTo(LOCATION_MOSCOW)));
+            mLocationTomskTextView.setText(String.format(mContext.getString(R.string.format_location_bearings),
+                    LOCATION_TOMSK.getLatitude(),
+                    LOCATION_TOMSK.getLongitude(),
+                    mLocation.bearingTo(LOCATION_TOMSK),
+                    mLocation.distanceTo(LOCATION_TOMSK)));
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        mLocationTextView = (TextView) rootView.findViewById(R.id.text_location);
+        mLocationTextView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if (mLocation != null) {
+                    openLocationOnMap(mLocation);
+                    return true;
+                }
+                return false;
+            }
+        });
+        mLocationTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentOverlay = SHOW_NOTHING;
+            }
+
+        });
+        mLocationMoscowTextView = (TextView) rootView.findViewById(R.id.text_location_moscow);
+        mLocationMoscowTextView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                openLocationOnMap(LOCATION_MOSCOW);
+                return true;
+            }
+        });
+        mLocationMoscowTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentOverlay = SHOW_MOSCOW;
+            }
+
+        });
+        mLocationTomskTextView = (TextView) rootView.findViewById(R.id.text_location_tomsk);
+        mLocationTomskTextView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                openLocationOnMap(LOCATION_TOMSK);
+                return true;
+            }
+        });
+        mLocationTomskTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentOverlay = SHOW_TOMSK;
+            }
+
+        });
+
+        mAzimuthMoscowTextView = (TextView) rootView.findViewById(R.id.text_azimuth_moscow);
+        mAzimuthTomskTextView = (TextView) rootView.findViewById(R.id.text_azimuth_tomsk);
+        mOrientationTextView = (TextView) rootView.findViewById(R.id.text_orientation);
+
+        mFrameLayout = (FrameLayout) rootView.findViewById(R.id.frame_for_overlay);
+        mOverlayView = new OverlayView(mContext, BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_delete));
+        mFrameLayout.addView(mOverlayView);
+
+        return rootView;
+    }
+
+    private void openLocationOnMap(Location location) {
+
+        String locationString = "geo:".concat(String.valueOf(location.getLatitude())).concat(",")
+                .concat(String.valueOf(location.getLongitude()));
+        Uri geoLocation = Uri.parse(locationString);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+
+        Log.i(LOG_TAG, locationString);
+
+        if (intent.resolveActivity(mContext.getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d(LOG_TAG, "Couldn't resolve");
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        updateLocationTextView();
+
+        // Register for network location updates
+        try {
+            if (null != mLocationManager.getProvider(LocationManager.NETWORK_PROVIDER)) {
+                mLocationManager.requestLocationUpdates(LocationManager
+                        .NETWORK_PROVIDER, POLLING_FREQ, MIN_DISTANCE, mLocationListener);
+            }
+            // Register for GPS location updates
+            if (null != mLocationManager.getProvider(LocationManager.GPS_PROVIDER)) {
+                mLocationManager.requestLocationUpdates(LocationManager
+                        .GPS_PROVIDER, POLLING_FREQ, MIN_DISTANCE, mLocationListener);
+            }
+        }
+        catch (SecurityException ex) {
+            ex.printStackTrace();
+        }
+
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+        mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
     @Override
     public void onSensorChanged(SensorEvent event) {
         int type = event.sensor.getType();
@@ -136,17 +285,17 @@ public class FragmentMain extends Fragment implements SensorEventListener {
                 mOrientationAverage = lowPass(orientationMatrix, mOrientationAverage);
 
                 // Assuming the device is on it's right side and camera away from user
-                double azimuth = Math.toDegrees(mOrientationAverage[0]) - 90;
+                double azimuth = Math.toDegrees(mOrientationAverage[0]) - ORIENTATION_CORRECTION;
                 double pitch = Math.toDegrees(mOrientationAverage[1]);
-                double roll = Math.toDegrees(mOrientationAverage[2]);
+                mRoll = Math.toDegrees(mOrientationAverage[2]);
                 azimuth = formatPiMinusPi(azimuth);
 
                 mOrientationTextView.setText(String.format(mContext.getString(R.string.format_sensor),
                         azimuth,
                         pitch,
-                        roll));
-                if (roll > 80 && roll < 100
-                        && pitch > -10 && pitch < 10) {
+                        mRoll));
+                if (mRoll > IDEAL_ROLL - ROLL_TOLERANCE && mRoll < IDEAL_ROLL + ROLL_TOLERANCE
+                        && pitch > -PITCH_TOLERANCE && pitch < PITCH_TOLERANCE) {
                     mOrientationTextView.setTextColor(Color.GREEN);
                 }
                 else {
@@ -155,17 +304,17 @@ public class FragmentMain extends Fragment implements SensorEventListener {
 
                 if (mLocation != null) {
 
-                    double moscowAzimuth = mLocation.bearingTo(LOCATION_MOSCOW) - azimuth;
-                    moscowAzimuth = formatPiMinusPi(moscowAzimuth);
+                    mAzimuthMoscow = mLocation.bearingTo(LOCATION_MOSCOW) - azimuth;
+                    mAzimuthMoscow = formatPiMinusPi(mAzimuthMoscow);
                     mAzimuthMoscowTextView.setText(String.format(mContext.getString(R.string.format_azimuth),
-                            moscowAzimuth));
-                    checkInFieldOfView(moscowAzimuth, mAzimuthMoscowTextView);
+                            mAzimuthMoscow));
+                    checkInFieldOfView(mAzimuthMoscow, mAzimuthMoscowTextView);
 
-                    double tomskAzimuth = mLocation.bearingTo(LOCATION_TOMSK) - azimuth;
-                    tomskAzimuth = formatPiMinusPi(tomskAzimuth);
+                    mAzimuthTomsk = mLocation.bearingTo(LOCATION_TOMSK) - azimuth;
+                    mAzimuthTomsk = formatPiMinusPi(mAzimuthTomsk);
                     mAzimuthTomskTextView.setText(String.format(mContext.getString(R.string.format_azimuth),
-                            tomskAzimuth));
-                    checkInFieldOfView(tomskAzimuth, mAzimuthTomskTextView);
+                            mAzimuthTomsk));
+                    checkInFieldOfView(mAzimuthTomsk, mAzimuthTomskTextView);
                 }
 
                 // Reset sensor event data arrays
@@ -173,12 +322,6 @@ public class FragmentMain extends Fragment implements SensorEventListener {
             }
         }
 
-    }
-
-    private double formatPiMinusPi(double angle) {
-        angle = (angle < -180) ? angle + 360 : angle;
-        angle = (angle > 180) ? angle - 360 : angle;
-        return angle;
     }
 
     private float[] lowPass(float[] orientationMatrix, float[] orientationAverage) {
@@ -191,8 +334,14 @@ public class FragmentMain extends Fragment implements SensorEventListener {
         return orientationAverage;
     }
 
+    private double formatPiMinusPi(double angle) {
+        angle = (angle < -180) ? angle + 360 : angle;
+        angle = (angle > 180) ? angle - 360 : angle;
+        return angle;
+    }
+
     private void checkInFieldOfView(double azimuth, TextView textView) {
-        if (azimuth > -30 && azimuth < 30) {
+        if (azimuth > -FIELD_OF_VIEW && azimuth < FIELD_OF_VIEW) {
             textView.setTextColor(Color.GREEN);
         }
         else {
@@ -203,108 +352,6 @@ public class FragmentMain extends Fragment implements SensorEventListener {
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         //nothing to see here
-    }
-
-    private void updateLocationTextView() {
-        if (mLocation != null) {
-            mLocationTextView.setText(String.format(mContext.getString(R.string.format_location),
-                    mLocation.getLatitude(),
-                    mLocation.getLongitude()));
-            mLocationMoscowTextView.setText(String.format(mContext.getString(R.string.format_location_bearings),
-                    LOCATION_MOSCOW.getLatitude(),
-                    LOCATION_MOSCOW.getLongitude(),
-                    mLocation.bearingTo(LOCATION_MOSCOW),
-                    mLocation.distanceTo(LOCATION_MOSCOW)));
-            mLocationTomskTextView.setText(String.format(mContext.getString(R.string.format_location_bearings),
-                    LOCATION_TOMSK.getLatitude(),
-                    LOCATION_TOMSK.getLongitude(),
-                    mLocation.bearingTo(LOCATION_TOMSK),
-                    mLocation.distanceTo(LOCATION_TOMSK)));
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-        mLocationTextView = (TextView) rootView.findViewById(R.id.text_location);
-        mLocationTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mLocation != null) {
-                    openLocationOnMap(mLocation);
-                }
-            }
-        });
-        mLocationMoscowTextView = (TextView) rootView.findViewById(R.id.text_location_moscow);
-        mLocationMoscowTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openLocationOnMap(LOCATION_MOSCOW);
-            }
-        });
-        mLocationTomskTextView = (TextView) rootView.findViewById(R.id.text_location_tomsk);
-        mLocationTomskTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openLocationOnMap(LOCATION_TOMSK);
-            }
-        });
-
-        mAzimuthMoscowTextView = (TextView) rootView.findViewById(R.id.text_azimuth_moscow);
-        mAzimuthTomskTextView = (TextView) rootView.findViewById(R.id.text_azimuth_tomsk);
-        mOrientationTextView = (TextView) rootView.findViewById(R.id.text_orientation);
-
-        return rootView;
-    }
-
-    private void openLocationOnMap(Location location) {
-
-        String locationString = "geo:".concat(String.valueOf(location.getLatitude())).concat(",")
-                .concat(String.valueOf(location.getLongitude()));
-        Uri geoLocation = Uri.parse(locationString);
-
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(geoLocation);
-
-        Log.i(LOG_TAG, locationString);
-
-        if (intent.resolveActivity(mContext.getPackageManager()) != null) {
-            startActivity(intent);
-        } else {
-            Log.d(LOG_TAG, "Couldn't resolve");
-        }
-    }
-
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        updateLocationTextView();
-
-        // Register for network location updates
-        try {
-            if (null != mLocationManager.getProvider(LocationManager.NETWORK_PROVIDER)) {
-                mLocationManager.requestLocationUpdates(LocationManager
-                        .NETWORK_PROVIDER, POLLING_FREQ, MIN_DISTANCE, mLocationListener);
-            }
-            // Register for GPS location updates
-            if (null != mLocationManager.getProvider(LocationManager.GPS_PROVIDER)) {
-                mLocationManager.requestLocationUpdates(LocationManager
-                        .GPS_PROVIDER, POLLING_FREQ, MIN_DISTANCE, mLocationListener);
-            }
-        }
-        catch (SecurityException ex) {
-            ex.printStackTrace();
-        }
-
-        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
-//        mLastUpdateAccelerometer = System.currentTimeMillis();
-        mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_UI);
-//        mLastUpdateMagnetometer = System.currentTimeMillis();
     }
 
     @Override
@@ -325,5 +372,89 @@ public class FragmentMain extends Fragment implements SensorEventListener {
         mSensorManager.unregisterListener(this);
 
         super.onPause();
+    }
+
+
+    private class OverlayView extends SurfaceView implements SurfaceHolder.Callback {
+
+        private final SurfaceHolder mSurfaceHolder;
+        private final Bitmap mBitmap;
+        private final Paint mPainter = new Paint();
+        private final int mBitmapSize;
+
+        private Thread mDrawingThread;
+
+        public OverlayView(Context context, Bitmap bitmap) {
+            super(context);
+
+            mBitmapSize = (int) getResources().getDimension(R.dimen.image_height_width);
+            this.mBitmap = Bitmap.createScaledBitmap(bitmap,
+                    mBitmapSize,
+                    mBitmapSize,
+                    false);
+
+            mPainter.setAntiAlias(true);
+
+            mSurfaceHolder = getHolder();
+            mSurfaceHolder.addCallback(this);
+        }
+
+        private void drawMarker(Canvas canvas) {
+            canvas.drawColor(Color.DKGRAY);
+            double azimuth = 1000; // intentionally unrealistic just to initialize
+            switch (mCurrentOverlay) {
+                case SHOW_NOTHING:
+                    return;
+                case SHOW_MOSCOW:
+                    azimuth = mAzimuthMoscow;
+                    break;
+                case SHOW_TOMSK:
+                    azimuth = mAzimuthTomsk;
+                    break;
+            }
+            float left = mFrameLayout.getWidth()/2.0f - mBitmapSize/2.0f;
+            if (mRoll > IDEAL_ROLL - ROLL_TOLERANCE && mRoll < IDEAL_ROLL + ROLL_TOLERANCE) {
+                left = left + ((float) ((mRoll - IDEAL_ROLL) / ROLL_TOLERANCE * mFrameLayout.getWidth() / 2.0));
+            }
+            else {
+                return;
+            }
+            float top = mFrameLayout.getHeight()/2.0f - mBitmapSize/2.0f;
+            if (azimuth > -FIELD_OF_VIEW && azimuth < FIELD_OF_VIEW) {
+                top = top - ((float) (azimuth / FIELD_OF_VIEW * mFrameLayout.getHeight() / 2.0));
+            }
+            else {
+                return;
+            }
+            canvas.drawBitmap(mBitmap, left, top, mPainter);
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width,int height) {
+            //nothing to see here
+        }
+
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            mDrawingThread = new Thread(new Runnable() {
+                public void run() {
+                    Canvas canvas = null;
+                    while (!Thread.currentThread().isInterrupted()) {
+                        canvas = mSurfaceHolder.lockCanvas();
+                        if (null != canvas) {
+                            drawMarker(canvas);
+                            mSurfaceHolder.unlockCanvasAndPost(canvas);
+                        }
+                    }
+                }
+            });
+            mDrawingThread.start();
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            if (null != mDrawingThread)
+                mDrawingThread.interrupt();
+        }
     }
 }
